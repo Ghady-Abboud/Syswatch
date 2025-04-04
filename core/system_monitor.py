9bd8c546541edfedab2f904ffb595f90c8804bb8
Ghady Abboud <abboudghady@gmail.com>
2025-04-04 14:41:21 -0400

    Added Cpu_temp tests

--- core/system_monitor.py
+++ core/system_monitor.py
@@ -11,3 +11,18 @@ def get_memory_usage():
 
 def get_cpu_load_average():
     return psutil.getloadavg()
+
+def get_cpu_temp():
+    if not hasattr(psutil, "sensors_temperatures"):
+        raise NotImplementedError("Temperature monitoring not supported on this system.")
+
+    temps =  {}
+    try:
+        sensor_data = psutil.sensors_temperatures()
+        if 'coretemp' in sensor_data:
+            for i,entry in enumerate(sensor_data['coretemp'][1:], 1):
+                temps[f"Core {i-1}"] = entry.current
+    except Exception as e:
+        print(f"Error retrieving CPU temperature: {e}")
+    print(temps)
+    return temps
--- tests/test_system_monitor.py
+++ tests/test_system_monitor.py
@@ -2,12 +2,14 @@ import unittest
 import sys
 import os
 
-
-
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from core.system_monitor import get_cpu_usage, get_memory_usage,get_cpu_load_average
-
+from core.system_monitor import (
+    get_cpu_usage,
+    get_memory_usage,
+    get_cpu_load_average,
+    get_cpu_temp,
+)
 class TestSystemMonitor(unittest.TestCase):
 
     def _assert_usage_value(self, get_usage_func):
@@ -27,3 +29,13 @@ class TestSystemMonitor(unittest.TestCase):
         cpu_load_average = get_cpu_load_average()
         self.assertIsInstance(cpu_load_average,tuple)
         self.assertIsInstance(cpu_load_average[0],float)
+
+    def test_get_cpu_temp(self):
+       temp = get_cpu_temp()
+       self.assertIsInstance(temp, dict)
+
+       for core,value in temp.items():
+           self.assertIn("Core",core)
+           self.assertIsInstance(value, float)
+           self.assertGreaterEqual(value, 0.0)
+           self.assertLessEqual(value, 100.0)
